00b3a214aef95230f209e36636299b46cf38b3d4
dnmarques <dn.marques@campus.fct.unl.pt>
2016-04-07 09:50:38 +0100

    Adding insert message form

---
+++ resources/views/messages/form.blade.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+            <div class="panel-heading">Answer to this topic</div>
+            @if(count($errors)>0)
+                @foreach($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            @endif
+            <div class="panel-body">
+                <form action="{{ Request::url() . '/messages' }}" method="POST" class="form-horizontal">
+                    {!! csrf_field() !!}
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">Message</label>
+
+                        <div class="col-sm-6">
+                            <input type="text" name="message" id="message" class="form-control">
+                        </div>
+                    </div>
+
+                    <!-- Add Message Button -->
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-6">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fa fa-plus"></i> Add Message
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/topics/messages.blade.php
+++ resources/views/topics/messages.blade.php
@@ -6,30 +6,31 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ $topic_title }}</div>
-
-                <div class="panel-body">
-                    @if(count($messages) > 0)
-                        <table class="table">
-                            <tr>
-                                <th>ID</th>
-                                <th>CONTENT</th>
-                                <th>WHO</th>
-                                <th>WHEN</th>
-                            </tr>
-                            @foreach($messages as $message)
-                            <tr>
-                                <td>{{ $message->id }}</td>
-                                <td>{{ $message->content }}</td>
-                                <td>{{ $message->name }}</td>
-                                <td>{{ $message->created_at }}</td>
-                            </tr>
-                            @endforeach
-                    @else
-                        <p>There are no messages in this topic</p>
-                    @endif
-                </div>
+                    <div class="panel-body">
+                        @if(count($messages) > 0)
+                            <table class="table">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>CONTENT</th>
+                                    <th>WHO</th>
+                                    <th>WHEN</th>
+                                </tr>
+                                @foreach($messages as $message)
+                                <tr>
+                                    <td>{{ $message->id }}</td>
+                                    <td>{{ $message->content }}</td>
+                                    <td>{{ $message->name }}</td>
+                                    <td>{{ $message->created_at }}</td>
+                                </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <p>There are no messages in this topic</p>
+                        @endif
+                    </div>
             </div>
         </div>
     </div>
+    @include('messages.form')
 </div>
 @endsection

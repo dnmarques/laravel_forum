e78108c91d0ff3b186bdf86520911fe3c6c28556
dnmarques <dn.marques@campus.fct.unl.pt>
2016-04-13 11:02:13 +0100

    Cashed everything

--- app/Repositories/MessagesRepository.php
+++ app/Repositories/MessagesRepository.php
@@ -9,26 +9,29 @@ use Illuminate\Support\Facades\Cache;
 
 class MessagesRepository {
 
-	public function updateContent($message_id, $topic_id, $content) {
-		$tags = ['topic-'.$topic_id.'-messages'];
+	public function updateContent($message_id, $topic_id, $content) { // DONE
+		$tags[0] = 'topic-'.$topic_id.'-message-'.$message_id.'-content';
+		$tags[1] = 'topic-'.$topic_id.'-message-'.$message_id;
+		$tags[2] = 'topic-'.$topic_id.'-messages';
 		$return = Message::where([['id', $message_id],['topic_id', $topic_id]])->update(['content' => $content]);
-		Cache::tags('topic-'.$topic_id.'-messages')->flush();
+		Cache::tags($tags)->flush();
 		return $return;
 
 	}
 
-	public function getMessageFromTopic($topic_id, $message_id) {
-		$key = 'topic-' . $topic_id . '-message-' . $message_id . '-content';
+	public function getMessageFromTopic($topic_id, $message_id) { // DONE
+		$key 	 = 'topic-'.$topic_id.'-message-'.$message_id.'-content';
 		$tags[0] = 'topic-'.$topic_id.'-message-'.$message_id.'-content';
 		$tags[1] = 'topic-'.$topic_id.'-message-'.$message_id;
 		$tags[2] = 'topic-'.$topic_id.'-messages';
+		$tags[3] = 'topic-'.$topic_id;
 		$return = Cache::tags($tags)->remember($key, 2, function() use($topic_id, $message_id) {
 			return Message::where([['topic_id', '=', $topic_id],['id', '=', $message_id]])->pluck('content');
 		});
 		return $return[0];
 	}
 
-	public function storeMessage(User $user, $topic_id, $content) {
+	public function storeMessage(User $user, $topic_id, $content) { // DONE
 		$counter = $this->getCounter($topic_id);
 
 		if($counter == null)
@@ -42,16 +45,16 @@ class MessagesRepository {
 				'user_id' => $user->id
 			]);
 		
-		$tags = ['topic-' . $message->topic_id . '-messages'];
+		$tags = ['topic-'.$message->topic_id.'-messages'];
 		Cache::tags($tags)->flush();
 	}
 
-	public function destroy(Message $message) {
-		// $key = 'topic-'. $message->topic_id . '-message-' . $message->id;
-		$tags = ['topic-' . $message->topic_id . '-messages'];
-		// Cache::forget($key); usar so se houver operacao de aceder apenas a 1 mensagem
+	public function destroy(Message $message) { // DONE
+		$tags[0] = 'topic-'.$message->topic_id.'-message-' . $message->id;
+		$tags[1] = 'topic-'.$message->topic_id.'-messages';
 		Cache::tags($tags)->flush();
-		return $message->delete();
+		Message::where([['topic_id', $message->topic_id], ['id', $message->id]])->delete();
+
 	}
 
 	private function getCounter($topic_id) {
--- app/Repositories/TopicsRepository.php
+++ app/Repositories/TopicsRepository.php
@@ -10,8 +10,9 @@ use App\Message;
 class TopicsRepository {
 
 	public function getTopicTitle($topic_id) {
-		$key = 'topic-'.$topic_id.'-title';
-		$tags = 'topic-'.$topic_id;
+		$key 	 = 'topic-'.$topic_id.'-title';
+		$tags[0] = 'topic-'.$topic_id.'-title';
+		$tags[1] = 'topic-'.$topic_id;
 		return Cache::tags($tags)->remember($key, 2, function() use($topic_id) {
 			$array = Topic::where('id', '=', $topic_id)->pluck('title');
 			return $array[0];
@@ -19,15 +20,17 @@ class TopicsRepository {
 	}
 
 	public function allWithUser() {
-		return Cache::remember('topics-list', 2, function() {
+		$key 	 = 'topics-list';
+		$tags[0] = 'topics-list';
+		return Cache::tags($tags)->remember($key, 2, function() {
 			return Topic::select('title', 'name', 'topics.id', 'topics.user_id')->
 					join('users', 'users.id', '=', 'topics.user_id')->get();
 		});
 	}
 
 	public function getAllMessagesFromTopic($topic_id) {
-		$tags = ['topic-' . $topic_id . '-messages'];
-		$key = 'topic-'. $topic_id. '-messages';
+		$key 	 = 'topic-'.$topic_id.'-messages';
+		$tags[0] = 'topic-'.$topic_id.'-messages';
 
 		return Cache::tags($tags)->remember($key, 2, function() use ($topic_id) {
 			return Topic::select('messages.*', 'users.name')
@@ -42,15 +45,17 @@ class TopicsRepository {
 		$topic = $user->topics()->create([
 				'title' => $title,
 			]);
-		Cache::forget('topics-list');
+		$tags[0] = 'topics-list';
+		Cache::tags($tags)->flush();
 		return $topic;
 	}
 
 	public function destroy(Topic $topic) {
 		Message::where('topic_id', '=', $topic->id)->delete();
 		$topic->delete();
-		Cache::forget('topics-list');
-		Cache::tags('topic-'.$topic->id)->flush();
+		$tags[0] = 'topics-list';
+		$tags[1] = 'topic-'.$topic->id;
+		Cache::tags($tags)->flush();
 	}
 
 	public function lastMessageId(Topic $topic) {
